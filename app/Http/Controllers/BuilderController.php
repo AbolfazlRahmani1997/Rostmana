796da609f026332662fe1fa5a6b2ecfcc1673b06
abolfazlrahmani1997 <abolfazlrahmani1997@gmail.com>
2020-11-16 17:22:54 +0330

    first app

--- app/Http/Controllers/BuilderController.php
+++ app/Http/Controllers/BuilderController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\builder;
 use Illuminate\Http\Request;
-use App\User;
 
-
-class UserController extends Controller
+class BuilderController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,10 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-       $user=User::first();
-       return $user->Profile->phonenumber;
-
-
+        //
     }
 
     /**
@@ -45,10 +41,10 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\builder  $builder
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(builder $builder)
     {
         //
     }
@@ -56,10 +52,10 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\builder  $builder
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(builder $builder)
     {
         //
     }
@@ -68,10 +64,10 @@ class UserController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\builder  $builder
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, builder $builder)
     {
         //
     }
@@ -79,10 +75,10 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\builder  $builder
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(builder $builder)
     {
         //
     }
